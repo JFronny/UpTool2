75c558b8c2550c44bfda76e7bd414de63dcf4569
CreepyCrafter24 <33260128+CreepyCrafter24@users.noreply.github.com>
2019-10-21 15:27:52 +0200

    Changed config files a bit

--- UpTool2/Properties/AssemblyInfo.cs
+++ UpTool2/Properties/AssemblyInfo.cs
@@ -6,11 +6,11 @@ using System.Runtime.InteropServices;
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
 [assembly: AssemblyTitle("UpTool2")]
-[assembly: AssemblyDescription("")]
+[assembly: AssemblyDescription("Download and update applications from UpTool repos")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("")]
+[assembly: AssemblyCompany("CreepyCrafter24")]
 [assembly: AssemblyProduct("UpTool2")]
-[assembly: AssemblyCopyright("Copyright ©  2019")]
+[assembly: AssemblyCopyright("Copyright © CreepyCrafter24 2019")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
@@ -32,5 +32,5 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Build and Revision Numbers
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.0.0.1")]
-[assembly: AssemblyFileVersion("1.0.0.1")]
+[assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyFileVersion("1.0.1.0")]
